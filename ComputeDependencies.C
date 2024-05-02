17d1ba74454186631fe72a2769059ee4f21ebf79
Daniela Paredes <danielakparedes@gmail.com>
2024-05-02 20:45:46 +0200

    Update ComputeDependencies.C

--- ComputeDependencies.C
+++ ComputeDependencies.C
@@ -13,7 +13,6 @@
 #include "TFile.h"
 #include "TLine.h"
 
-
 #include "../../BaselineFramework/Tools/Yields.C"
 #include "../../BaselineFramework/Tools/Channel.C"
 #include "../../BaselineFramework/Tools/EventCut.C"
@@ -58,6 +57,7 @@ class qRatios{
        TString m_folder;
 
 };
+
 void ComputeDependencies(){
 
   gROOT->LoadMacro("MyStyle.C");
@@ -86,13 +86,10 @@ void ComputeDependencies(){
      njets_dep={"ttbar_"+bin+"_nJets_le4","ttbar_"+bin+"_nJets_ge5","ttbar_"+bin+"_nJets_ge0"};
   }
 
-
   TString norm="#epsilon_{QmisID}("+bin+")";
   if (bin=="plus") norm="#epsilon_{QmisID}(default(+))";
 
-
   TString sample=process+"_"+bin+"_nJets_ge0";//sample used for normalization. 
-
 
   /*
   qRatios qRatio_Ht_range(Ht_dep,"ratio_Ht","ratio_Ht_range",sample,"#epsilon_{QmisID}(H_{T} dep.)/"+norm,process);
@@ -116,8 +113,7 @@ void ComputeDependencies(){
   qRatio_ttbar.Execute();
   */
 
-
- return;
+  return;
 }
 
 qRatios::qRatios(std::vector<TString> files,TString process,TString var,TString normalize_sample,TString ylabel,TString folder):
@@ -132,15 +128,15 @@ qRatios::qRatios(std::vector<TString> files,TString process,TString var,TString 
   m_folder(folder),
   m_sample_to_normalization(normalize_sample)
 {}
+
 qRatios::~qRatios()
 {
-
   /*for (std::map<TString,TH2F*>::iterator it=m_histos2D.begin(); it!=m_histos2D.end(); ++it){
         delete it->second;
    }
  */
-
 }
+
 void qRatios::Execute(){
 
   GetHistos2D(); //Extract 2D histos
@@ -151,10 +147,9 @@ void qRatios::Execute(){
 
   GetRatios();
 
-
   return;
-
 }
+
 void qRatios::GetRatios(){
 
  std::cout << "## Getting ratio plots " << std::endl;
@@ -162,29 +157,24 @@ void qRatios::GetRatios(){
  std::vector<TH1D*> denominator=m_histos1D[m_sample_to_normalization];
  //std::vector<TString> legend=m_histo_legend[m_sample_to_normalization];
 
-
  TCanvas *pC = 0; 
  if (nPtbins==4){
      pC=new TCanvas();
      pC->Divide(2,2);
-
  }
  else if (nPtbins>4){
    pC=new TCanvas(m_var,m_var,1600,800);
    pC->Divide(3,2); 
 }
  
- for (unsigned i=0; i<nPtbins; i++){
+for (unsigned i=0; i<nPtbins; i++){
 
       pC->cd(i+1);
-
 
       TLegend *pLeg=new TLegend(0.6,0.6,0.9,0.9);
 
       int counter=0;
       for (std::map<TString,std::vector<TH1D*> >::iterator it=m_histos1D.begin(); it!=m_histos1D.end(); ++it){
-
-      
           
             TH1D *pH=(TH1D*)it->second[i]->Clone();
             pH->Divide(denominator[i]); 
@@ -197,43 +187,40 @@ void qRatios::GetRatios(){
             pH->SetMarkerStyle(24);
             pH->SetMarkerColor(col[counter]);             
 
-
             TString local=GetRatioLegend(it->first);
 
-           if (it->first!=m_sample_to_normalization){
+            if (it->first!=m_sample_to_normalization){
 
 
-            if (counter==0) pH->Draw("pL e1");
-            else pH->Draw("L e1 same");
-            pLeg->AddEntry(pH,local,"pl");
+               if (counter==0) pH->Draw("pL e1");
+               else pH->Draw("L e1 same");
+               pLeg->AddEntry(pH,local,"pl");
 
-            if (counter==0) {
+               if (counter==0) {
 
-               TLine *pLine=new TLine(pH->GetXaxis()->GetXmin(),1.,pH->GetXaxis()->GetXmax(),1.);
-               pLine->SetLineWidth(2);
-               pLine->Draw("same");
+                  TLine *pLine=new TLine(pH->GetXaxis()->GetXmin(),1.,pH->GetXaxis()->GetXmax(),1.);
+                  pLine->SetLineWidth(2);
+                  pLine->Draw("same");
 
-            }
+               }// end counter
 
-            counter+=1;
-           }
-      }
+               counter+=1;
+             } // end normalization
+      }// end loop
 
       pLeg->Draw("same");
 
-     MiniTreeAnalyzer newanalyzer;
-     newanalyzer.GetATLAS("Internal",0.185,0.88,false,0.05);
-     newanalyzer.GetLabel(0.186,0.83,"13 TeV",0.04);
-     newanalyzer.GetLabel(0.186,0.78,m_histo_legend[m_sample_to_normalization][i],0.04);
+      MiniTreeAnalyzer newanalyzer;
+      newanalyzer.GetATLAS("Internal",0.185,0.88,false,0.05);
+      newanalyzer.GetLabel(0.186,0.83,"13 TeV",0.04);
+      newanalyzer.GetLabel(0.186,0.78,m_histo_legend[m_sample_to_normalization][i],0.04);
 
-
-     pC->SaveAs(m_folder+"/"+m_var+".pdf");
-
+      pC->SaveAs(m_folder+"/"+m_var+".pdf");
  }
-
 
  return;
 }
+
 void qRatios::GetHistos2D(){
 
    for (unsigned int i=0; i<m_names.size(); i++){ 
@@ -260,11 +247,10 @@ void qRatios::GetHistos2D(){
    newanalyzer.GetATLAS("Internal",0.185,0.88,false,0.05);
    newanalyzer.GetLabel(0.186,0.81,"13 TeV",0.04);
 
-
- return;
+   return;
 }
-void qRatios::GetHistos1D(){
 
+void qRatios::GetHistos1D(){
     
    for (std::map<TString,TH2F*>::iterator it=m_histos2D.begin(); it!=m_histos2D.end(); ++it){
 
@@ -279,7 +265,6 @@ void qRatios::GetHistos1D(){
 
         std::cout<<"- Will project histos from  : " << it->first << std::endl;
         
-
         for (unsigned int i=1; i <= pH2->GetNbinsY(); i++){
             std::cout << "-- Proyecting histogram in bin : " << i << std::endl;   
             std::string bin = std::to_string(i);
@@ -289,7 +274,6 @@ void qRatios::GetHistos1D(){
             TH1D *pH=(TH1D*)pH2->ProjectionX(name,i,i,"e");
             pH->SetDirectory(0);
             pH->GetYaxis()->SetTitle("#epsilon_{QmisID}");
-
 
             TString legend=GetLegend(pH2,i);
 
@@ -301,70 +285,66 @@ void qRatios::GetHistos1D(){
             /*TCanvas *pC = new TCanvas;
             pC->cd();
             pH->Draw();*/
-
-
         }
+
         m_histos1D[it->first]=v_pH1D;
         m_histo_legend[it->first]=v_legend;
-
    }
 
- return;
+   return;
 }
+
 TString qRatios::GetRatioLegend(TString name){
 
-TString leg=name;
+   TString leg=name;
 
-MiniTreeAnalyzer analyzer;
-std::vector<std::string> tokens;
+   MiniTreeAnalyzer analyzer;
+   std::vector<std::string> tokens;
 
-std::string local(name.Data());
+   std::string local(name.Data());
 
-analyzer.tokenizeString(local,'_',tokens);
+   analyzer.tokenizeString(local,'_',tokens);
 
+   TString str_min="";
+   TString str_max="";
+   TString var=TString(tokens[2]);
 
- TString str_min="";
- TString str_max="";
- TString var=TString(tokens[2]);
-
- for (unsigned int i=0; i<tokens.size(); i++){
+   for (unsigned int i=0; i<tokens.size(); i++){
 
       TString str=tokens[i];
       if (str.Contains("ge")) str_min=str;
       else if (str.Contains("le")) str_max=str;
- }
+   }
  
-TString min=str_min;
-TString max=str_max;
+   TString min=str_min;
+   TString max=str_max;
 
-std::cout <<"## DEBUG :" << min << " max: " << max << std::endl;
+   std::cout <<"## DEBUG :" << min << " max: " << max << std::endl;
 
+   if (str_min!="") min=GetValue(str_min,"ge");
+   if (str_max!="") max=GetValue(str_max,"le");
 
-if (str_min!="") min=GetValue(str_min,"ge");
-if (str_max!="") max=GetValue(str_max,"le");
+   if (str_min !="" && str_max != "") leg=var+" #in ["+min+","+max+"]";
+   else if (str_min !="") leg=var+" #geq "+min;
+   else if (str_max !="") leg=var+" #leq "+max;
 
+   if (name.Contains("met") || name.Contains("Ht")) leg=leg+" GeV";
 
-if (str_min !="" && str_max != "") leg=var+" #in ["+min+","+max+"]";
-else if (str_min !="") leg=var+" #geq "+min;
-else if (str_max !="") leg=var+" #leq "+max;
+   if (leg.Contains("met")) leg.ReplaceAll("met","E_{T}^{miss}");
+   if (leg.Contains("Ht")) leg.ReplaceAll("Ht","H_{T}");
 
+   std::cout << "-- leg :" << leg << std::endl;
 
-if (name.Contains("met") || name.Contains("Ht")) leg=leg+" GeV";
-
-
-if (leg.Contains("met")) leg.ReplaceAll("met","E_{T}^{miss}");
-if (leg.Contains("Ht")) leg.ReplaceAll("Ht","H_{T}");
-
-std::cout << "-- leg :" << leg << std::endl;
-
-return leg;
+   return leg;
 }
+
 TString qRatios::GetValue(TString str,TString out){
 
    str.ReplaceAll(out,"");
 
    return str;
 }
+
 TString qRatios::GetLegend(TH2F *pH,int bin){
 
   TString legend="p_{T}^{l}";
@@ -378,9 +358,10 @@ TString qRatios::GetLegend(TH2F *pH,int bin){
 
   return legend;
 }
+
 void qRatios::Plot1D(){
 
- for (std::map<TString,std::vector<TH1D*> >::iterator it=m_histos1D.begin(); it!=m_histos1D.end(); ++it){ 
+  for (std::map<TString,std::vector<TH1D*> >::iterator it=m_histos1D.begin(); it!=m_histos1D.end(); ++it){ 
 
      std::cout << "- Plotting histos for : " << it->first << std::endl;
 
@@ -412,24 +393,21 @@ void qRatios::Plot1D(){
 
          pH->GetName();
 
-     }
+     }//end for
      pLeg->Draw("same");
 
      TString name="mc16d: Z+jets";
      if (temp.Contains("ttbar")) name="mc16d: t#bar t"; 
-
 
      MiniTreeAnalyzer newanalyzer;
      newanalyzer.GetATLAS("Internal",0.185,0.88,false,0.05);
      newanalyzer.GetLabel(0.186,0.83,"13 TeV",0.04);
      newanalyzer.GetLabel(0.186,0.78,name,0.04);
 
-
      pC->SaveAs("Rates1D/"+it->first+".pdf");
 
- }
+  }
 
-
- return;
+  return;
 }
 
